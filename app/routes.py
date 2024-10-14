5d65d025de3a6307fdbc3eaf458854e032ffce4b
muturi254 <petermuturi74@gmail.com>
2024-10-14 16:03:13 +0300

    add template to view function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
+
 from app import app
 
 # views
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    user = {"username": "peter"}
+    title = "home"
+    posts = [
+        {
+            "author": {"username": "John Doe"},
+            "body": "Once a man is a man is a man"
+        },
+        {
+            "author": {"username": "Jane Kimani"},
+            "body": "for the beauty on beauts"
+        }
+    ]
+    return render_template('index.html', title=title, user=user, posts=posts)
